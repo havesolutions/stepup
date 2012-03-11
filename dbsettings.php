8786d97c2d7fbddc46da0bd7783a23f9601d0b26
Dinesh <dineshdhoom@gmail.com>
2012-03-11 21:46:44 +0530

    Initializing the repo

---
+++ dbsettings.php
@@ -0,0 +1,27 @@
+
+<?php
+
+/*
+***************************************************
+*** Online Examination System                   ***
+***---------------------------------------------***
+*** License: GNU General Public License V.3     ***
+*** Author: Manjunath Baddi                     ***
+*** Title: Database Settings                    ***
+***************************************************
+*/
+
+//This is the name of your server where the MySQL database is running
+$dbserver="localhost";
+
+//username of the MySQL server
+$dbusername="root";
+
+//password
+
+$dbpassword="";
+
+//database name of the online Examination system
+$dbname="wesearch_stepup_new";
+
+?>
